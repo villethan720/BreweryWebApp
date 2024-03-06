2d9deea2d993e0f26999083c12a08ec9f99f2cc1
villethan720 <93165624+villethan720@users.noreply.github.com>
2024-03-06 15:59:57 -0800

    Create startup.cs

---
+++ BrewHub webApp/startup.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrewHub_webApp{
+     public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<webserviceModel, webserviceModel>();
+        }
+    }
+}
