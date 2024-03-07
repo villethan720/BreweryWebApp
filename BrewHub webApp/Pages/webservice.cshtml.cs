e9fe012f6faa5a91cfe265282c73e3c12020d53f
eperkun-ppu <139075758+eperkun-ppu@users.noreply.github.com>
2024-03-07 11:03:15 -0500

    Change table layout

--- BrewHub webApp/Pages/webservice.cshtml.cs
+++ BrewHub webApp/Pages/webservice.cshtml.cs
@@ -1,49 +1,3 @@
-// using System.Collections.Generic;
-// using System.Net.Http;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Mvc.RazorPages;
-// using Newtonsoft.Json.Linq;
-// using Microsoft.Extensions.DependencyInjection;
-
-// public class webserviceModel : PageModel
-// {
-//     private readonly HttpClient _httpClient;
-//     public webserviceModel(HttpClient httpClient)
-//     {
-//         _httpClient = httpClient;
-//     }
-
-//     public Dictionary<string, string> webservice { get; set;}
-
-//     public async Task OnGetAsync()
-//     {
-//         webservice = await GetDataFromApiAsync();
-//     }
-
-//     private async Task<Dictionary<string, string>> GetDataFromApiAsync()
-//     {
-//         var response = await _httpClient.GetAsync("https://brewhubapi.onrender.com/api/v1/brewhub"); //Put our websevice url in the GetAsync()
-//         response.EnsureSuccessStatusCode();
-
-//         var responseContent = await response.Content.ReadAsStringAsync();
-//         var responseData = JArray.Parse(responseContent);
-
-//         var data = new Dictionary<string, string>();
-//         foreach (var item in responseData)
-//         {
-//             Console.WriteLine(item);
-//             var jObject = (JObject)item;
-//             foreach (var property in jObject.Properties())
-//             {
-//                 data.Add(property.Name, property.Value.ToString());
-//             }
-//         }
-
-//         return data;
-//     }
-// }
-
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -59,14 +13,14 @@ public class webserviceModel : PageModel
     }
 
     // Updated to use an array of tuples
-    public (string Key, string Value)[] webservice { get; set; }
+    public DBEntry[] webservice { get; set; }
 
     public async Task OnGetAsync()
     {
         webservice = await GetDataFromApiAsync();
     }
 
-    private async Task<(string, string)[]> GetDataFromApiAsync()
+    private async Task<DBEntry[]> GetDataFromApiAsync()
     {
         var response = await _httpClient.GetAsync("https://brewhubapi.onrender.com/api/v1/brewhub");
         response.EnsureSuccessStatusCode();
@@ -74,18 +28,63 @@ public class webserviceModel : PageModel
         var responseContent = await response.Content.ReadAsStringAsync();
         var responseData = JArray.Parse(responseContent);
 
-        var dataList = new List<(string, string)>();
+        var dataList = new List<DBEntry>();
         foreach (var item in responseData)
         {
             var jObject = (JObject)item;
+
+            int id = -1, established = -1, priceLevel = -1;
+            string name = "", address = "", city = "", state = "", website = "";
+
             foreach (var property in jObject.Properties())
             {
-                // Add each property as a tuple to the list
-                dataList.Add((property.Name, property.Value.ToString()));
+                if(property.Name.Equals("id")) {
+                    id = property.Value.ToObject<int>();
+                } else if(property.Name.Equals("name")) {
+                    name = property.Value.ToString();
+                } else if(property.Name.Equals("address")) {
+                    address = property.Value.ToString();
+                } else if(property.Name.Equals("city")) {
+                    city = property.Value.ToString();
+                } else if(property.Name.Equals("state")) {
+                    state = property.Value.ToString();
+                } else if(property.Name.Equals("established")) {
+                    established = property.Value.ToObject<int>();
+                } else if(property.Name.Equals("website")) {
+                    website = property.Value.ToString();
+                } else if(property.Name.Equals("price_level")) {
+                    priceLevel = property.Value.ToObject<int>();
+                }
             }
+
+            DBEntry entry = new DBEntry(id, name, address, city, state, established, website, priceLevel);
+            dataList.Add(entry);
         }
 
         // Convert the list to an array before returning
         return dataList.ToArray();
+    }
+
+    public class DBEntry {
+        public static string[] headers = ["Name", "Address", "City", "State", "Established", "Website", "Price Level"];
+        public int id;
+        public string name;
+        public string address;
+        public string city;
+        public string state;
+        public int established;
+        public string website;
+        public int priceLevel;
+
+        public DBEntry(int id, string name, string address, string city, string state, int established, string website, int priceLevel) {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.established = established;
+            this.website = website;
+            this.priceLevel = priceLevel;
+        }
     }
 }
