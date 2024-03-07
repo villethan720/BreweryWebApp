7090b80ddc16533a510c2d8b1532e2019cb113d1
eperkun-ppu <139075758+eperkun-ppu@users.noreply.github.com>
2024-03-06 19:42:47 -0500

    Add section in "Our Team"

--- BrewHub webApp/obj/Debug/net8.0/BrewHub webApp.AssemblyInfo.cs
+++ BrewHub webApp/obj/Debug/net8.0/BrewHub webApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446db470fa07070278a4359043817bb49456d58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4de1cfdf6ecec67ee330937cbbe293da588156")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
