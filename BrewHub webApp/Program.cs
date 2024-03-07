8f87ee2a2be27422d938b26a9c15e90b55f503d6
eperkun-ppu <139075758+eperkun-ppu@users.noreply.github.com>
2024-03-06 23:20:39 -0500

    Added semi-working webserviceModel page.  Ethan and I are going to fix it up

--- BrewHub webApp/Program.cs
+++ BrewHub webApp/Program.cs
@@ -1,7 +1,10 @@
+using BrewHub_webApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
--- BrewHub webApp/obj/Debug/net8.0/BrewHub webApp.AssemblyInfo.cs
+++ BrewHub webApp/obj/Debug/net8.0/BrewHub webApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4de1cfdf6ecec67ee330937cbbe293da588156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7090b80ddc16533a510c2d8b1532e2019cb113d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrewHub webApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
